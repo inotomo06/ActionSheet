3e1e22e0a4fd06b85cc0eaa85bac33ccc7f542fc
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-08-23 09:21:48 +0900

    Initial Commit

---
+++ ActionSheet/ActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheetApp.swift
+//  ActionSheet
+//
+//  Created by cmStudent on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
